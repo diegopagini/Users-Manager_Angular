da90941bee86692f038db184fad17c23bfcd76db
unknown <diegopagini777@gmail.com>
2020-11-11 15:56:00 -0300

    ya tengo la cantidad de usuario, yuupii

--- src/app/public/dashboard/list/list.component.ts
+++ src/app/public/dashboard/list/list.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Users, UsersData } from 'src/app/core/interfaces/users.interface';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  usersLength: number;
+  currentInfo: Array<UsersData>;
 
+  API: string = 'https://randomuser.me/api/?results=100';
+
+  constructor(private apiService: ApiService) { }
   ngOnInit(): void {
+    this.getUsers()
   }
 
+  getUsers() {
+    this.apiService.getUsers().subscribe((response: Users) => {
+      this.usersLength = response.results.length;
+      console.log('Cantidad de Usuarios', this.usersLength);
+    })
+  }
 }
